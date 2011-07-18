0bb713a5f3201d754849d5a7a35cb0246d519d00
Les Orchard <l.m.orchard@pobox.com>
2011-07-18 18:57:18 -0400

    Make jingo.Loader properly fall back to other TEMPLATE_LOADERS
    - jingo.Loader raises TemplateDoesNotExist when excluded app
      encountered, which causes fallback to the next loader listed in
      settings.TEMPLATE_LOADERS
    
    - renamed setting DJANGO_TEMPLATE_APPS -> JINGO_EXCLUDE_APPS
    
    - tests updated to ensure top-level override of app-level templates
      works for both Jinja and Django loaders

--- jingo/__init__.py
+++ jingo/__init__.py
@@ -5,8 +5,9 @@ import logging
 
 from django import http
 from django.conf import settings
+from django.template.base import TemplateDoesNotExist
 from django.template.context import get_standard_processors
-from django.template.loaders.app_directories import Loader as AppLoader
+from django.template.loader import BaseLoader
 from django.utils.importlib import import_module
 from django.utils.translation import trans_real
 
@@ -165,15 +166,14 @@ class Template(object):
         return self.template.render(context_dict)
 
 
-class Loader(AppLoader):
+class Loader(BaseLoader):
     is_usable = True
 
     def load_template(self, template_name, template_dirs=None):
         if hasattr(template_name, 'rsplit'):
             app = template_name.rsplit('/')[0]
-            if app in getattr(settings, 'DJANGO_TEMPLATE_APPS', []):
-                return super(Loader, self).load_template(
-                        template_name, template_dirs)
+            if app in getattr(settings, 'JINGO_EXCLUDE_APPS', []):
+                raise TemplateDoesNotExist(template_name)
 
         template = env.get_template(template_name)
         return Template(template), template.filename
