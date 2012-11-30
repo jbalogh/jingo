33d37b9e26e0f0aa061a2114296567efeb07911c
James Socol <me@jamessocol.com>
2012-11-30 12:40:52 -0500

    Django dropped direct_to_template.

--- jingo/tests/test_views.py
+++ jingo/tests/test_views.py
@@ -1,17 +1,9 @@
 from django.utils import translation
-from django.views.generic.simple import direct_to_template
 
 from mock import patch, sentinel
 from nose.tools import eq_
 
 from jingo import get_env, render_to_string
-
-
-def test_direct_to_template():
-    response = direct_to_template(sentinel.request,
-                                  'jinja_app/test_nonoverride.html',
-                                  {'x': 1})
-    eq_('HELLO', response.content)
 
 
 def test_template_substitution_crash():
