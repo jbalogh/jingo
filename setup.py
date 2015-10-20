d6274a467be88c32528abd441e9e0f60698e1dec
James Socol <me@jamessocol.com>
2015-10-20 08:59:16 -0400

    Attribution cleanup

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     version='0.8',
     description='An adapter for using Jinja2 templates with Django.',
     long_description=open('README.rst').read(),
-    author='Jeff Balogh',
-    author_email='jbalogh@mozilla.com',
+    author='James Socol',
+    author_email='me@jamessocol.com',
     url='http://github.com/jbalogh/jingo',
     license='BSD',
     packages=['jingo'],
@@ -17,18 +17,16 @@ setup(
     classifiers=[
         'Development Status :: 4 - Beta',
         'Environment :: Web Environment',
-        # I don't know what exactly this means, but why not?
-        'Environment :: Web Environment :: Mozilla',
         'Framework :: Django',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ]
 )
